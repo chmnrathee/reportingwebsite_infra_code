090f8209c70304d762efe7c0850e4e68ad9539f2
Chaman Rathee <chaman.rathee@fireeye.com>
2021-09-03 11:47:33 +0530

    added tf_code for container_registry

---
+++ tf_code/container_registry.tf
@@ -0,0 +1,15 @@
+# DataBase Values
+postgresql-admin-user    = "Samplechaman@ascentdjangopgdb"
+postgresql-admin-password = "SamplePass"
+postgresql-version        = "11"
+postgresql-sku-name       = "B_Gen5_1"
+postgresql-storage        = "5432"
+
+# Container registry
+registry-name = "test_registry"
+sku_tier = "Standard"
+sku_size = "S1"
+
+# App Service 
+app_service_plan_name = "reportinwebitePlan"
+web_app_name = "reportingwebsite"
